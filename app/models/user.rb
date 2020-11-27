ed2511f1d4938f4680f6141bf6de06a80162e576
Mees Barthen <mees@barthen.com>
2020-11-27 11:37:24 +0100

    fixed the show page and full name bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,15 @@ class User < ApplicationRecord
   has_many :instruments
   has_many :bookings
   has_many :bookings_as_owner, through: :instruments, source: :bookings
+  validates_presence_of :first_name, :last_name
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   def full_name
-    first_name + ' ' + last_name
+    if first_name && last_name
+      first_name + ' ' + last_name
+    else
+      "undenfind"
+    end
   end
 end
